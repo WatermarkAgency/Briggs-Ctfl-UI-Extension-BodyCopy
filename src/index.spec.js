1d69b209d106d792242c6249e08abeecc2c0181b
Kennan Smith <kennan@watermarkadvertising.net>
2020-03-19 10:39:18 -0600

    switched 'body' to 'copy'

--- src/index.spec.js
+++ src/index.spec.js
@@ -14,7 +14,7 @@ const sdk = {
   entry: {
     fields: {
       title: { getValue: jest.fn(), setValue: jest.fn() },
-      body: { getValue: jest.fn(), setValue: jest.fn() },
+      copy: { getValue: jest.fn(), setValue: jest.fn() },
       image: { getValue: jest.fn(), setValue: jest.fn() },
       imagePosition: { getValue: jest.fn(), setValue: jest.fn() },
       hasImage: { getValue: jest.fn(), setValue: jest.fn() }
@@ -31,20 +31,20 @@ describe('App', () => {
 
   it('should read a values from entry.fields.*', () => {
     sdk.entry.fields.title.getValue.mockReturnValue('title-value');
-    sdk.entry.fields.body.getValue.mockReturnValue('body-value');
+    sdk.entry.fields.copy.getValue.mockReturnValue('copy-value');
     sdk.entry.fields.hasImage.getValue.mockReturnValue(true);
     sdk.entry.fields.image.getValue.mockReturnValue('image-value');
     sdk.entry.fields.imagePosition.getValue.mockReturnValue('imagePosition-value');
     const { getByTestId } = renderComponent(sdk);
 
     expect(getByTestId('field-title').value).toEqual('title-value');
-    expect(getByTestId('field-body').value).toEqual('body-value');
+    expect(getByTestId('field-copy').value).toEqual('copy-value');
     expect(getByTestId('field-image').value).toEqual('image-value');
 
-    fireEvent.change(getByTestId('field-body'), {
-      target: { value: 'new-body-value' }
+    fireEvent.change(getByTestId('field-copy'), {
+      target: { value: 'new-copy-value' }
     });
 
-    expect(sdk.entry.fields.body.setValue).toHaveBeenCalledWith('new-body-value');
+    expect(sdk.entry.fields.copy.setValue).toHaveBeenCalledWith('new-copy-value');
   });
 });
